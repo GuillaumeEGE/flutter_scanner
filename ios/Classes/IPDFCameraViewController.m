7089827bb4b514a9f6ab1d821764cb6963660065
GuillaumeEGE <151170125+GuillaumeEGE@users.noreply.github.com>
2025-03-07 17:48:08 +0700

    Update IPDFCameraViewController.m

--- ios/Classes/IPDFCameraViewController.m
+++ ios/Classes/IPDFCameraViewController.m
@@ -105,7 +105,7 @@
   }
   if (!device) return;
 
-  self.imageDetectionConfidence = 0.0;
+  self.imageDetectionConfidence = 0.5;
 
   AVCaptureSession *session = [[AVCaptureSession alloc] init];
   self.captureSession = session;
