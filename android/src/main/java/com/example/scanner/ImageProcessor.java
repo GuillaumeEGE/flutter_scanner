b7b23bbea5fbeeab7be4c62b8bf6a4cdf4db8fad
GuillaumeEGE <151170125+GuillaumeEGE@users.noreply.github.com>
2025-03-07 17:44:44 +0700

    Update ImageProcessor.java

--- android/src/main/java/com/example/scanner/ImageProcessor.java
+++ android/src/main/java/com/example/scanner/ImageProcessor.java
@@ -73,7 +73,7 @@ public class ImageProcessor extends Handler {
     private Point[] mPreviewPoints;
     private ResultPoint[] qrResultPoints;
     private int numOfSquares = 0;
-    private int numOfRectangles = 10;
+    private int numOfRectangles = 5;
     private boolean noGrayscale;
 
     public ImageProcessor(Looper looper, Handler uiHandler, OpenNoteCameraView mainActivity, Context context) {
