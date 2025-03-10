07ac13c783c3b2194dca451cb3de541353c602a0
GuillaumeEGE <151170125+GuillaumeEGE@users.noreply.github.com>
2025-03-10 10:30:42 +0700

    Update ScannerViewManager.java

--- android/src/main/java/com/example/scanner/ScannerViewManager.java
+++ android/src/main/java/com/example/scanner/ScannerViewManager.java
@@ -100,11 +100,12 @@ uiThreadHandler.postAtFrontOfQueue(runnable );
 
         view.setDocumentAnimation(documentAnimation);
 
-        String overlayColor;
+        /*String overlayColor;
         if(params.containsKey("overlayColor")){
             overlayColor = (String ) params.get("overlayColor");
             view.setOverlayColor(overlayColor);
-        }
+        }*/
+            view.setOverlayColor("#19AA3A");
 
         int detectionCountBeforeCapture;
         if(params.containsKey("detectionCountBeforeCapture")){
